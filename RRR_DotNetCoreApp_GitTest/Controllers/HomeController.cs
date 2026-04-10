d05414fbab23e160fb05e29a365ab85d80d33e63
rajatrai159 <rajatrai159@gmail.com>
2026-04-10 13:07:42 +0530

    Add project files.

---
+++ RRR_DotNetCoreApp_GitTest/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RRR_DotNetCoreApp_GitTest.Models;
+
+namespace RRR_DotNetCoreApp_GitTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RRR_DotNetCoreApp_GitTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RRR_DotNetCoreApp_GitTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
